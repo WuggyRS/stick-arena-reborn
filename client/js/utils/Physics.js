5c3ff58d0dd715fbbdc1fbd65129ecce1d3bf131
WuggyRS <52111974+WuggyRS@users.noreply.github.com>
2023-01-08 10:35:10 -0600

    Improve shooting collision detection accuracy

--- client/js/utils/Physics.js
+++ client/js/utils/Physics.js
@@ -4,18 +4,87 @@ class Physics {
     const corner2 = rect.topRight;
     const corner3 = rect.bottomLeft;
     const corner4 = rect.bottomRight;
+    const vertices = [corner1, corner2, corner3, corner4];
 
-    const distance1 = this.distanceToLine(circle, corner1, corner2);
-    const distance2 = this.distanceToLine(circle, corner2, corner3);
-    const distance3 = this.distanceToLine(circle, corner3, corner4);
-    const distance4 = this.distanceToLine(circle, corner4, corner1);
+    const rectangle = { vertices: vertices };
+    circle.radius = radius;
 
-    if (distance1 <= radius || distance2 <= radius || distance3 <= radius || distance4 <= radius) {
-      return true;
-    }
-
-    return false;
+    const { overlap, mtv } = this.circleToPolygon(circle, rectangle);
+    return overlap;
   }
+
+  static dotProduct = (v1, v2) => v1.x * v2.x + v1.y * v2.y;
+
+  static magnitude = v => Math.sqrt(Math.pow(v.x, 2) + Math.pow(v.y, 2));
+
+  static normalizeVector = v => {
+    const m = this.magnitude(v);
+    return m == 0 ? v : { x: v.x / m, y: v.y / m };
+  }
+
+  static getEdges = vertices =>
+    vertices.reduce((a, v, i, l) => {
+      const n = (i + 1) % l.length
+      return [...a, { x: l[n].x - l[i].x, y: l[n].y - l[i].y }];
+    }, []);
+
+  static getNormals = edges =>
+    edges.reduce((a, v) => {
+      return [...a, this.normalizeVector({ x: -v.y, y: v.x })];
+    }, []);
+
+  static getProjections = (normal, vertices) =>
+    Object.values(vertices).reduce(
+      (projection, vertex) => {
+        const dot = this.dotProduct(normal, vertex)
+        return {
+          ...projection,
+          min: Math.min(projection.min, dot),
+          max: Math.max(projection.max, dot)
+        }
+      },
+      { min: Infinity, max: -Infinity }
+    );
+
+  static getCircleAxes = (circle, polygon) => {
+    const axes = [];
+    polygon.vertices.forEach((v, i, l) => {
+      axes.push(this.normalizeVector({ x: v.x - circle.x, y: v.y - circle.y }));
+    })
+    return axes;
+  };
+
+  // https://github.com/leondejong/js-sat
+  static circleToPolygon = (a, b) => {
+    const v = b.vertices
+    const normals = [...this.getNormals(this.getEdges(v)), ...this.getCircleAxes(a, b)]
+    let mtv = { x: Infinity, y: Infinity }
+
+    const overlap = normals.every(n => {
+      const dot = this.dotProduct(n, { x: a.x, y: a.y });
+      const p1 = { min: dot - a.radius, max: dot + a.radius };
+      const p2 = this.getProjections(n, v);
+      const c1 = p1.min < p2.max && p1.min > p2.min;
+      const c2 = p2.min < p1.max && p2.min > p1.min;
+
+      if (c1 || c2) {
+        let m = Infinity;
+        if (c1) {
+          m = p2.max - p1.min;
+        } else if (c2) {
+          m = p2.min - p1.max;
+        }
+        const vector = { x: n.x * m, y: n.y * m };
+        if (this.magnitude(vector) < this.magnitude(mtv)) {
+          mtv = vector // minimum translation vector
+        }
+      }
+
+      return c1 || c2;
+    })
+
+    return { overlap, mtv: overlap ? mtv : { x: 0, y: 0 } };
+  };
 
   static distanceToLine(point, linePoint1, linePoint2) {
     const lineLength = this.distance(linePoint1, linePoint2);
